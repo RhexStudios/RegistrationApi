50cb80a197af9fe50a6682e7c88d13a28867edae
henriquegouveia1 <henriquegs002@gmail.com>
2023-07-18 17:19:44 -0300

    Reajustem em configs do DB

--- src/Config/DbConfig.js
+++ src/Config/DbConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   dialect: 'mysql',
   host: '127.0.0.1',
-  port: '3333',
+  port: '3306',
   username: 'root',
   password: 'ROOT',
   database: 'resgistration_rhex_teste',
--- src/Infra/database.js
+++ src/Infra/database.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize');
-const config = require('../Config/DbConfig');
+const dbConfig = require('../Config/DbConfig');
 
 const connection = new Sequelize(
-  variables.dbConfig.database,
-  variables.dbConfig.username,
-  variables.dbConfig.password,
-  variables.dbConfig
+  dbConfig.database,
+  dbConfig.username,
+  dbConfig.password,
+  dbConfig
 );
 
 module.exports = connection;
